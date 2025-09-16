f8ec548138fe5e1cd340a2bff476ffef8921c2c2
duoduoxu <duoduoxu@tencent.com>
2025-09-16 22:06:36 +0800

    feat: move style

--- apps/admin111/src/main.ts
+++ apps/admin111/src/main.ts
@@ -12,11 +12,11 @@ import Table from '@pureadmin/table';
 // import PureDescriptions from "@pureadmin/descriptions";
 
 // 引入重置样式
-import './style/reset.scss';
+import '@repo/style/reset.scss';
 // 导入公共样式
-import './style/index.scss';
+import '@repo/style/index.scss';
 // 一定要在main.ts中导入tailwind.css，防止vite每次hmr都会请求src/style/index.scss整体css文件导致热更新慢的问题
-import './style/tailwind.css';
+import '@repo/style/tailwind.css';
 import 'element-plus/dist/index.css';
 // 导入字体图标
 import './assets/iconfont/iconfont.js';
