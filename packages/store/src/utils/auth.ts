15dc3b88967a9e099691c7bfe10ff36f451053b7
duoduoxu <duoduoxu@tencent.com>
2025-09-16 17:12:22 +0800

    feat: add auth for store utils

---
+++ packages/store/src/utils/auth.ts
@@ -0,0 +1,94 @@
+import Cookies from 'js-cookie';
+import { storageLocal, isString, isIncludeAllChildren } from '@pureadmin/utils';
+import { multipleTabsKey, TokenKey, userKey } from '@repo/constants/user';
+import type { DataInfo } from '@repo/types/user';
+import { useUserStoreHook } from '../modules/user';
+
+/**
+ * @description 设置`token`以及一些必要信息并采用无感刷新`token`方案
+ * 无感刷新：后端返回`accessToken`（访问接口使用的`token`）、`refreshToken`（用于调用刷新`accessToken`的接口时所需的`token`，`refreshToken`的过期时间（比如30天）应大于`accessToken`的过期时间（比如2小时））、`expires`（`accessToken`的过期时间）
+ * 将`accessToken`、`expires`、`refreshToken`这三条信息放在key值为authorized-token的cookie里（过期自动销毁）
+ * 将`avatar`、`username`、`nickname`、`roles`、`permissions`、`refreshToken`、`expires`这七条信息放在key值为`user-info`的localStorage里（利用`multipleTabsKey`当浏览器完全关闭后自动销毁）
+ */
+export function setToken(data: DataInfo<Date>) {
+  let expires = 0;
+  const { accessToken, refreshToken } = data;
+  const { isRemembered, loginDay } = useUserStoreHook();
+  expires = new Date(data.expires).getTime(); // 如果后端直接设置时间戳，将此处代码改为expires = data.expires，然后把上面的DataInfo<Date>改成DataInfo<number>即可
+  const cookieString = JSON.stringify({ accessToken, expires, refreshToken });
+
+  expires > 0
+    ? Cookies.set(TokenKey, cookieString, {
+        expires: (expires - Date.now()) / 86400000,
+      })
+    : Cookies.set(TokenKey, cookieString);
+
+  Cookies.set(
+    multipleTabsKey,
+    'true',
+    isRemembered
+      ? {
+          expires: loginDay,
+        }
+      : {}
+  );
+
+  function setUserKey({ avatar, username, nickname, roles, permissions }: any) {
+    useUserStoreHook().SET_AVATAR(avatar);
+    useUserStoreHook().SET_USERNAME(username);
+    useUserStoreHook().SET_NICKNAME(nickname);
+    useUserStoreHook().SET_ROLES(roles);
+    useUserStoreHook().SET_PERMS(permissions);
+    storageLocal().setItem(userKey, {
+      refreshToken,
+      expires,
+      avatar,
+      username,
+      nickname,
+      roles,
+      permissions,
+    });
+  }
+
+  if (data.username && data.roles) {
+    const { username, roles } = data;
+    setUserKey({
+      avatar: data?.avatar ?? '',
+      username,
+      nickname: data?.nickname ?? '',
+      roles,
+      permissions: data?.permissions ?? [],
+    });
+  } else {
+    const avatar =
+      storageLocal().getItem<DataInfo<number>>(userKey)?.avatar ?? '';
+    const username =
+      storageLocal().getItem<DataInfo<number>>(userKey)?.username ?? '';
+    const nickname =
+      storageLocal().getItem<DataInfo<number>>(userKey)?.nickname ?? '';
+    const roles =
+      storageLocal().getItem<DataInfo<number>>(userKey)?.roles ?? [];
+    const permissions =
+      storageLocal().getItem<DataInfo<number>>(userKey)?.permissions ?? [];
+    setUserKey({
+      avatar,
+      username,
+      nickname,
+      roles,
+      permissions,
+    });
+  }
+}
+
+/** 是否有按钮级别的权限（根据登录接口返回的`permissions`字段进行判断）*/
+export const hasPerms = (value: string | Array<string>): boolean => {
+  if (!value) return false;
+  const allPerms = '*:*:*';
+  const { permissions } = useUserStoreHook();
+  if (!permissions) return false;
+  if (permissions.length === 1 && permissions[0] === allPerms) return true;
+  const isAuths = isString(value)
+    ? permissions.includes(value)
+    : isIncludeAllChildren(value, permissions);
+  return isAuths ? true : false;
+};
