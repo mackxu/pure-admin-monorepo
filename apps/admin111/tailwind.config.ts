6bf0684797ee8d1d668c26337ea1e35157867144
duoduoxu <duoduoxu@tencent.com>
2025-09-19 14:48:07 +0800

    feat: 删除router-helper

--- apps/admin111/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{vue,ts}',
-    '../../pages/base/src/**/*.{vue,ts}',
-  ],
-} satisfies Config;
